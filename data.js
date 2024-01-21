e729111b90d5f6f3702e1a43f5355f6f6853bbaa
vashviper <sheraj.hussein@gmail.com>
2024-01-22 00:04:55 +0800

    Added JSON Bin functionality + Save button

--- data.js
+++ data.js
@@ -1,63 +1,66 @@
+// at the top of `data.js`
+const BASE_JSON_BIN_URL = "https://api.jsonbin.io/v3/b";
+const BIN_ID = "65ad3521dc7465401897dbb6";
+const MASTER_KEY = "$2a$10$64dTsCnOhcZOxrDwSgyf8OW1Q/w2uUYZjAAV2zoCVREzkZqQ5CKua";
+
+
+
+
 let todos = [];
 
 function addTodo(todos, name, urgency) {
-    let newTodo = {
-        // id is unique
-        id: Math.floor(Math.random() * 100 + 1), // Random number from 1 - 100
-        name: name,
-        urgency: urgency
-    };
-    todos.push(newTodo);
+  let newTodo = {
+    id: Math.floor(Math.random() * 100 + 1),
+    name: name,
+    urgency: urgency
+  };
+  todos.push(newTodo);
 }
 
-function modifyTask(todos, id, newTaskName, newUrgency) { // id = 30
-    let task = null;
-
-    for (let t of todos) {
-        if (t.id == id) {
-            task = t;
-            break
-        }
+function modifyTask(todos, id, newTaskName, newUrgency) {
+  let task = null;
+  for (let t of todos) {
+    if (t.id == id) {
+      task = t;
     }
-
-    if (task) {
-        task.name = newTaskName;
-        task.urgency = newUrgency;
-    } else {
-        console.log("Task is not found modify");
-    }
+  }
+  if (task) {
+    task.name = newTaskName;
+    task.urgency = newUrgency;
+  } else {
+    console.log("Task is not found");
+  }
 }
 
-function deleteTask(todos, id) { 
-    let indexToDelete = null; 
-    // loop through todos array
-    for (let i = 0; i < todos.length; i++) {
-        if (todos[i].id == id) {
-            indexToDelete = i;
-        }
+function deleteTask(todos, id) {
+  let indexToDelete = null;
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      indexToDelete = i;
+      break;
     }
-
-    // deleting of item from todos array
-    if (indexToDelete !== null) {
-        todos.splice(indexToDelete, 1);
-    } else {
-        console.log("Task is not found delete");
-    }
+  }
+  if (indexToDelete !== null) {
+    todos.splice(indexToDelete, 1);
+  } else {
+    console.log("Task is not found");
+  }
 }
 
+// ...add at the end of `data.js`
+async function loadTasks() {
+    const response = await axios.get(BASE_JSON_BIN_URL + "/" + BIN_ID + "/latest");
+    console.log(response.data)
+    return response.data.record;
+  }
 
-function deleteAllTaskWithId(todos, id) {
-    let indexToDeleteArray = []
-
-    for (let i = 0; i < todos.length; i++) {
-        if (todos[i].id == id) {
-            indexToDeleteArray.push(i)
-        }
-    }
-
-    for (let i = indexToDeleteArray.length - 1; i >= 0; i--) {
-        let indexToDelete = indexToDeleteArray[i]
-        todos.splice(indexToDelete, 1);
-    }
-    
-}
+  async function saveTasks(todos) {
+    const response = await axios.put(`${BASE_JSON_BIN_URL}/${BIN_ID}`, todos, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Master-Key": MASTER_KEY
+      }
+    });
+    return response.data;
+  
+  }
--- script.js
+++ script.js
@@ -1,85 +1,70 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // all the code here
-    function main() {
-        //let todos = []; // store all the todos
-    
-        // Event listeners
-        const form = document.querySelector("#todo-form");
-        form.addEventListener('submit', function(event) {
-          event.preventDefault(); // This stops the form from being submitted the traditional way
-    
-          const taskNameInput = document.querySelector("#taskName");
-          const taskName = taskNameInput.value;
-    
-          const taskUrgencySelect = document.querySelector("#taskUrgency");
-          const taskUrgency = taskUrgencySelect.value;
-    
-          if (taskName) {
-            addTodo(todos, taskName, taskUrgency);
+document.addEventListener('DOMContentLoaded', function() {
+
+    async function main() {
+      let todos = await loadTasks(); 
+  
+      const addTodoButton = document.querySelector("#addTodo");
+      addTodoButton.addEventListener('click', function() {
+        const taskNameInput = document.querySelector("#taskName")
+        const taskName = taskNameInput.value;
+  
+        const taskUrgencySelect = document.querySelector("#taskUrgency");
+        const taskUrgency = taskUrgencySelect.value;
+  
+        if (taskName) {
+          addTodo(todos, taskName, taskUrgency);
+          renderTodos(todos);
+          taskNameInput.value = '';
+        }
+      });
+  
+      const saveButton = document.querySelector("#save-btn");
+      saveButton.addEventListener("click", async function() {
+      saveTasks(todos);
+    })
+  
+      // add three todos
+      addTodo(todos, "Walk the dog", 5);
+      addTodo(todos, "Clean the room", 3);
+      addTodo(todos, "Pay the bill", 2);
+      renderTodos(todos);
+    }
+  
+  
+    function renderTodos(todos) {
+      todoList.innerHTML = '';
+      for (let todo of todos) {
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        li.innerHTML = `
+                  ${todo.name} <span class="badge  bg-primary">${todo.urgency}</span>
+                  <button class="btn edit-btn btn-success btn-sm">Edit</button>
+                  <button class="btn delete-btn btn-danger btn-sm">Delete</button>
+                  
+              `;
+  
+        todoList.appendChild(li);
+  
+        // select the edit button which we just created
+        li.querySelector(".edit-btn").addEventListener('click', function() {
+          const newName = prompt("Enter the new task name: ", todo.name);
+          const newUrgency = prompt("Enter the new urgency:", todo.urgency);
+          modifyTask(todos, todo.id, newName, newUrgency);
+          renderTodos(todos);
+        });
+  
+        // allow deleting
+        li.querySelector(".delete-btn").addEventListener('click', function() {
+          const confirmation = confirm("Do you want to delete the task: " + todo.name + "?");
+          if (confirmation) {
+            deleteTask(todos, todo.id);
             renderTodos(todos);
-            taskNameInput.value = '';
           }
         });
-       
-       
-        const todoList = document.querySelector("#todoList");
-        todoList.addEventListener('click', function(event) {
-            // Check if the clicked element is the 'Edit' button
-            if (event.target.classList.contains('edit-btn')) {
-                const todoId = parseInt(event.target.dataset.taskId);
-                const todo = todos.find(t => t.id === todoId);
-        
-                const newName = prompt("Enter the new task name: ", todo.name);
-                const newUrgency = prompt("Enter the new urgency:", todo.urgency);
-        
-                modifyTask(todos, todo.id, newName, newUrgency);
-                renderTodos(todos);
-            }
-        
-            // Check if the clicked element is the 'Delete' button
-            if (event.target.classList.contains('delete-btn')) {
-                const todoId = parseInt(event.target.dataset.taskId);
-        
-                const confirmDelete = confirm("Are you sure you want to delete this task?");
-                if (confirmDelete) {
-                    deleteTask(todos, todoId);
-                    renderTodos(todos);
-                }
-            }
-        });
-        
+  
+      }
     }
-    
-    
-    function addSampleTasks() {
-        addTodo(todos, "Walk the dog", "low")
-        addTodo(todos, "Clean the room", "medium");
-        addTodo(todos, "Pay the bill", "high");
-    }
-
-
-    function renderTodos(todos) {
-        const todoList = document.querySelector("#todoList");
-        // clearing old todo list
-        todoList.innerHTML = '';
-
-        for (let todo of todos) {
-
-            // loop through todos and create li element with todo details
-            const li = document.createElement('li');
-            li.className = 'list-group-item d-flex justify-content-between align-items-center';
-            li.innerHTML = `
-                    ${todo.name} <span class="badge bg-primary">${todo.urgency}</span>
-                    <button data-task-id=${todo.id} class="btn edit-btn btn-success btn-sm">Edit</button>
-                    <button data-task-id=${todo.id} class="btn delete-btn btn-danger btn-sm">Delete</button>
-                  `;
-            todoList.appendChild(li);
-
-        }
-    }
-
-
-    addSampleTasks() 
-    renderTodos(todos)
+  
     main();
-});
+  });
+  
